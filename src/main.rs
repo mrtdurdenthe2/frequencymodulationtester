72bd94534578b13b2617789e4b25ef05b5a2f2e8
mrtdurdenthe2 <kai.wil.business@gmail.com>
2025-05-20 23:33:45 +0100

    AGI achieved internally

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,19 @@ fn main() {
     eframe::run_native("My egui App", native_options, Box::new(|cc| Ok(Box::new(MyEguiApp::new(cc)))));
 }
 
+#[derive(Default, Debug, Clone, Copy)]
+pub enum LengthUnit {
+    #[default]
+    Ns,
+    Us,
+    Ms,
+}
+
 #[derive(Default)]
-struct MyEguiApp {}
+struct MyEguiApp {
+    graphxsize: String,
+    unit: LengthUnit,
+}
 
 impl MyEguiApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
@@ -19,6 +30,11 @@ impl eframe::App for MyEguiApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Hello World!");
+
+            ui.horizontal(|ui| {
+                ui.label("Graph Size horizontal");
+                ui.text_edit_singleline(&mut self.graphxsize);
+            });
         });
     }
 }
