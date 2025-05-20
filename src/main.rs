efb54f88753c0a2d3439b1bf916fd7bb18f80ac1
mrtdurdenthe2 <kai.wil.business@gmail.com>
2025-05-20 22:27:51 +0100

    got egui working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use eframe::{egui};
+
+
 fn main() {
-    println!("Hello, world!");
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("My egui App", native_options, Box::new(|cc| Ok(Box::new(MyEguiApp::new(cc)))));
+}
+
+#[derive(Default)]
+struct MyEguiApp {}
+
+impl MyEguiApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for MyEguiApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+        });
+    }
 }
